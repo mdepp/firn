201dc8c0fc1cbfe5c4979dbe671ebfa3630b2a56
rain <31737634+mdepp@users.noreply.github.com>
2023-09-20 15:21:15 -0600

    Initial basic terminal

---
+++ src/child.rs
@@ -0,0 +1,112 @@
+use encoding_rs::{CoderResult, SHIFT_JIS};
+use iced::{
+    futures::{
+        channel::mpsc::{self, Sender},
+        join, SinkExt, StreamExt,
+    },
+    subscription, Subscription,
+};
+use log::debug;
+use std::{
+    future::{pending, Future},
+    process::Stdio,
+};
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWriteExt};
+
+const READ_BUF_SIZE: usize = 100;
+const CHANNEL_BUF_SIZE: usize = 100;
+const SHELL: &str = "/usr/bin/bash";
+
+pub fn connect() -> Subscription<OutputEvent> {
+    struct Connect;
+
+    subscription::channel(
+        std::any::TypeId::of::<Connect>(),
+        CHANNEL_BUF_SIZE,
+        |mut send_output| async move {
+            let (send_input, mut recv_input) = mpsc::channel(CHANNEL_BUF_SIZE);
+            send_output
+                .send(OutputEvent::Connected(send_input))
+                .await
+                .unwrap();
+
+            debug!("Connecting to shell...");
+            let mut shell_process = tokio::process::Command::new(SHELL)
+                .arg("-i")
+                .stdout(Stdio::piped())
+                .stderr(Stdio::piped())
+                .stdin(Stdio::piped())
+                .spawn()
+                .unwrap();
+            debug!("Connected to shell.");
+
+            let stdout = shell_process.stdout.take().unwrap();
+            let stderr = shell_process.stderr.take().unwrap();
+            let mut stdin = shell_process.stdin.take().unwrap();
+
+            let stdout_future = decode_output(stdout, |text| async {
+                debug!("Read stdout: {text:?}");
+                let mut set_output = send_output.clone();
+                set_output.send(OutputEvent::Stdout(text)).await.unwrap();
+            });
+            let stderr_future = decode_output(stderr, |text| async {
+                debug!("Read stderr: {text:?}");
+                let mut send_output = send_output.clone();
+                send_output.send(OutputEvent::Stdout(text)).await.unwrap();
+            });
+
+            let stdin_future = (|| async move {
+                debug!("Waiting for input messages...");
+                loop {
+                    match recv_input.next().await {
+                        Some(InputEvent::Stdin(text)) => {
+                            debug!("Write stdin: {text:?}");
+                            stdin.write(text.as_bytes()).await.unwrap();
+                        }
+                        None => break,
+                    }
+                }
+            })();
+
+            join!(stdout_future, stderr_future, stdin_future);
+
+            pending::<()>().await;
+            unreachable!();
+        },
+    )
+}
+
+async fn decode_output<T: AsyncRead, F: Future>(bytestream: T, mut cb: impl FnMut(String) -> F) {
+    let mut bytestream = Box::pin(bytestream);
+    let mut decoder = SHIFT_JIS.new_decoder_without_bom_handling();
+    let decode_buf_size = decoder.max_utf8_buffer_length(READ_BUF_SIZE).unwrap();
+
+    let mut readbuf = [0u8; READ_BUF_SIZE];
+    let decodebuf_full = String::from_utf8(vec![b'\0'; decode_buf_size]).unwrap();
+
+    let mut last = false;
+    while !last {
+        let nbytes = bytestream.read(&mut readbuf).await.unwrap();
+        last = nbytes == 0;
+        debug!("Read {} bytes: {:?}", nbytes, &readbuf[..nbytes]);
+        let mut decodebuf = decodebuf_full.clone();
+        let (result, nwritten, nread, replaced) =
+            decoder.decode_to_str(&readbuf[..nbytes], &mut decodebuf, last);
+        debug!("Decoded (result={result:?} nwritten={nwritten} nread={nread} replaced={replaced}: {:?}", &decodebuf[..nwritten]);
+        // Can't have OutputFull result since decode_buf_size was set sufficiently large
+        assert!(result == CoderResult::InputEmpty);
+        cb(String::from(&decodebuf[..nwritten])).await;
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum InputEvent {
+    Stdin(String),
+}
+
+#[derive(Debug, Clone)]
+pub enum OutputEvent {
+    Connected(Sender<InputEvent>),
+    Stdout(String),
+    Stderr(String),
+}
---
+++ src/main.rs
@@ -0,0 +1,93 @@
+mod child;
+
+use iced::event::{Event, Status};
+use iced::futures::channel::mpsc::Sender;
+use iced::subscription;
+use iced::widget::text;
+use iced::{executor, keyboard};
+use iced::{Application, Command, Element, Settings, Subscription, Theme};
+use log::debug;
+
+struct Firn {
+    text: String,
+    child_sender: Option<Sender<child::InputEvent>>,
+    theme: Theme,
+}
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    ApplicationEvent(Event),
+    ChildEvent(child::OutputEvent),
+}
+
+impl Application for Firn {
+    type Message = Message;
+    type Theme = Theme;
+    type Executor = executor::Default;
+    type Flags = ();
+
+    fn new(_: Self::Flags) -> (Self, Command<Message>) {
+        (
+            Self {
+                text: "".into(),
+                child_sender: None,
+                theme: Theme::Dark,
+            },
+            Command::none(),
+        )
+    }
+
+    fn title(&self) -> String {
+        String::from("Firn Terminal")
+    }
+
+    fn view(&self) -> Element<Message> {
+        text(self.text.clone()).into()
+    }
+
+    fn update(&mut self, message: Message) -> Command<Message> {
+        debug!("Recv message: {message:?}");
+        match message {
+            Message::ChildEvent(child::OutputEvent::Stdout(text)) => {
+                self.text += &text;
+            }
+            Message::ChildEvent(child::OutputEvent::Stderr(text)) => {
+                self.text += &text;
+            }
+            Message::ChildEvent(child::OutputEvent::Connected(sender)) => {
+                self.child_sender = Some(sender);
+            }
+            Message::ApplicationEvent(Event::Keyboard(keyboard::Event::CharacterReceived(ch))) => {
+                if let Some(child_sender) = self.child_sender.as_mut() {
+                    debug!("Send character to shell: {ch}");
+                    child_sender
+                        .try_send(child::InputEvent::Stdin(ch.into()))
+                        .unwrap();
+                }
+            }
+            _ => {}
+        };
+        Command::none()
+    }
+
+    fn subscription(&self) -> Subscription<Message> {
+        Subscription::batch([
+            child::connect().map(|event| Message::ChildEvent(event)),
+            subscription::events_with(|event, status| match (&event, status) {
+                (Event::Keyboard(_), Status::Ignored) => Some(Message::ApplicationEvent(event)),
+                _ => None,
+            }),
+        ])
+    }
+
+    fn theme(&self) -> Theme {
+        self.theme.clone()
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    env_logger::init();
+
+    Firn::run(Settings::default())?;
+    Ok(())
+}
